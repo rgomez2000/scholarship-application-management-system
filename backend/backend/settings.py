49274302b2a952eeaf287f2a9875365fcccad45c
starsvoyage <idiazvachier@arizona.edu>
2024-12-07 23:17:06 -0700

    Added pagination feature

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,6 +64,8 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': (
     #     'rest_framework.permissions.IsAuthenticated',
     # ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
